cbd494bd062257d1da7350173c955295c7b14db2
amineceyda <amineceyda33@gmail.com>
2022-04-07 16:52:04 +0300

    Metot Overloading eklendi

---
+++ CS 101/MetotOverloading.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MetotOverloading
+{ /*
+* Metot Overloading Nedir?
+Method overloading yani metotların aşırı yüklenmesi
+metot imzasının değiştirilerek aynı isimdeki metodun birden
+farklı versiyonun yaratılmasıdır.
+Metot imzası 
+metotAdı + parametresayisi + parametre tipleri
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi = "9999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonuc);//out değişkeninin bir ilk değeri olmak zorunda değildir. 
+            Console.WriteLine(toplamSonuc);
+
+
+            //Metot Overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Ceyda", " Tan");
+        }
+
+
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
